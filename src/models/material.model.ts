23f224ccfc993756bcaf1cd69384ae1de7854eae
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-07-27 21:33:40 +0530

    ADD: material model

---
+++ src/models/material.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableMaterialStatusEnum } from "../constant";
+
+const materialSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: AvailableMaterialStatusEnum,
+      required: true,
+    },
+
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Material = mongoose.model("Material", materialSchema);
